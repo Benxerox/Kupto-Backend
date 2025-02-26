0f8b8948b3707315ca628be5df85d9808bebd01c
Benxerox <benxerox2020@gmail.com>
2025-02-26 21:26:42 +0300

    mnbc

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -439,40 +439,9 @@ const emptyCart = asyncHandler(async(req, res)=>{
   }
 });
 
-const getProductTitleById = async (productId) => {
-  try {
-    // Find the product by ID
-    const product = await Product.findById(productId);
-    if (!product) {
-      throw new Error('Product not found');
-    }
-    return product.title; // Return the product title
-  } catch (error) {
-    throw new Error('Error retrieving product title: ' + error.message);
-  }
-};
-
 
 // Helper function to generate HTML for the receipt
-const generateReceiptHtml = async (order, shippingInfo, orderItems, totalPrice, paymentInfo) => {
-  const orderItemsHtml = await Promise.all(orderItems.map(async (item) => {
-    try {
-      const productTitle = await getProductTitleById(item.product); // Extract product title
-      return `
-        <li>
-          Product: ${productTitle}<br>
-          Quantity: ${item.quantity}<br>
-          Price: ${item.price}<br>
-          Size: ${item.size}<br>
-          Color: ${item.color}<br>
-          Instructions: ${item.instruction || 'None'}
-        </li>
-      `;
-    } catch (error) {
-      return `<li>Error fetching product details: ${error.message}</li>`;
-    }
-  }));
-
+const generateReceiptHtml = (order, shippingInfo, orderItems, totalPrice, paymentInfo) => {
   return `
     <h1>Order Receipt</h1>
     <h3>Order Number: ${order._id}</h3>
@@ -480,9 +449,19 @@ const generateReceiptHtml = async (order, shippingInfo, orderItems, totalPrice, 
     <p>Address: ${shippingInfo.firstName}  ${shippingInfo.lastName}</p>
     <p>Address: ${shippingInfo.address}</p>
 
+    
     <h3>Order Items:</h3>
     <ul>
-      ${orderItemsHtml.join('')}
+      ${orderItems.map(item => `
+        <li>
+          Product: ${item.product}<br>
+          Quantity: ${item.quantity}<br>
+          Price: ${item.price}<br>
+          Size: ${item.size}<br>
+          Color: ${item.color}<br>
+          Instructions: ${item.instruction || 'None'}
+        </li>
+      `).join('')}
     </ul>
     <h3>Total Price: ${totalPrice}</h3>
     <h3>Payment Method: ${paymentInfo.paymentMethod}</h3>
