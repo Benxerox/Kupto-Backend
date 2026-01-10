988f944b33f46a60fb14ce948d9162d789dc1bb1
Kupto <kupto2020@gmail.com>
2026-01-10 07:38:57 -0800

    nowww

--- routes/uploadPostRoute.js
+++ routes/uploadPostRoute.js
@@ -1,16 +1,23 @@
+
+
+// routes/uploadPostRoute.js
 const express = require("express");
-const { uploadImages } = require("../controller/uploadCtrl");
+const { uploadImages, deleteImages } = require("../controller/uploadCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const { uploadPostPhoto } = require("../middlewares/uploadImages");
 
 const router = express.Router();
 
+// ✅ POST images for posts — ORIGINAL SIZE (no sharp resize middleware)
 router.post(
   "/",
   authMiddleware,
   isAdmin,
-  uploadPostPhoto.array("images", 20), // ✅ NO resize here
+  uploadPostPhoto.array("images", 10),
   uploadImages
 );
 
+router.delete("/delete-img/:id", authMiddleware, isAdmin, deleteImages);
+
 module.exports = router;
+
