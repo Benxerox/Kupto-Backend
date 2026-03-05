38fce978b6b04f5876024ad58d4cd7d592074b85
Kupto <kupto2020@gmail.com>
2026-03-05 02:48:49 -0800

    newww

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -1,6 +1,16 @@
 
 
 // config/jwtToken.js
+/*
 const jwt = require("jsonwebtoken");
 const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1m" });
+module.exports = { generateToken };
+*/
+
+// config/jwtToken.js
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "15m" });
+
 module.exports = { generateToken };
